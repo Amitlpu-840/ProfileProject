f343bf6f386059fe4039a11ee943107d77f18b95
Amit Giri <thisisag840@gmail.com>
2023-05-27 16:34:23 +0530

    added post method

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Express = require("express");
+const bodyParser = require("body-parser");
 
 const app = Express();
-
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   //   res.send(`
@@ -16,6 +18,11 @@ app.get("/contact", (req, res) => {
   res.send(`<h1>contact harsha at mob. no. 984124142</h1>
   <a href="/" ><button>home</button> </a>
   `);
+});
+
+app.post("/", (req, res) => {
+  // console.log(req.body);
+  res.send("hello "+ req.body.name1+ "year = " + req.body.year);
 });
 
 app.listen(port, () => {
